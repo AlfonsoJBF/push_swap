60ee575e41b992d365eb8b77d7e58ef35c7614f2
Mikel Tapiador <mtapiado@c1r4s1.42madrid.com>
2026-06-29 12:18:19 +0200

    libreria algoritmos hasta sort_five

---
+++ include/algorithms.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithms.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtapiado <mtapiado@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/29 10:19:42 by mtapiado          #+#    #+#             */
+/*   Updated: 2026/06/29 12:09:54 by mtapiado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALGORITHMS_H
+# define ALGORITHMS_H
+
+# include "stack.h"
+
+void	sort_two(t_program *p);
+void	sort_three(t_program *p);
+void	sort_four(t_program *p);
+void	sort_five(t_program *p);
+
+#endif
