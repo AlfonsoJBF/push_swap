ed1687add9fda0a426e1ad6b957570791538d510
AlfonsoJBF <alfonsojbfraguio@gmail.com>
2026-06-30 15:05:58 +0200

    rework: main adapted to new parse

--- src/main/main.c
+++ src/main/main.c
@@ -6,128 +6,64 @@
 /*   By: albustos <albustos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/21 01:22:22 by albustos          #+#    #+#             */
-/*   Updated: 2026/06/30 02:16:56 by albustos         ###   ########.fr       */
+/*   Updated: 2026/06/30 14:58:22 by albustos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/debug.h"
-#include "../../include/libraries.h"
-#include "../../include/stack.h"
-#include "../../include/operations.h"
-#include "../../include/parse.h"
-#include "../../include/algorithms.h"
-#include <stdio.h>
 
-// int main(int argc, char **argv)
-// {
-// 	int *nums;
-// 	int nums_size;
-// 	t_program *p;
-
-// 	parse_args(argc, argv, &nums, &nums_size);
-// 	if(!nums || !nums_size)
-// 	{
-// 		write(1, "Error", 5);
-// 		return (1);
-// 	}
-// 	p = ft_program_init(nums, nums_size);
-// 	if (!p)
-// 		return (1);
-// 	print_program(p);
-// 	pa(p);
-// 	print_program(p);
-// 	pa(p);
-// 	print_program(p);
-// 	pa(p);
-// 	print_program(p);
-// 	sa(p);
-// 	print_program(p);
-// 	ra(p);
-// 	print_program(p);
-// 	rra(p);
-// 	print_program(p);
-// 	pb(p);
-// 	print_program(p);
-// 	sb(p);
-// 	print_program(p);
-// 	rb(p);
-// 	print_program(p);
-// 	rrb(p);
-// 	print_program(p);
-// 	ss(p);
-// 	print_program(p);
-// 	rr(p);
-// 	print_program(p);
-// 	rrr(p);
-// 	print_program(p);
-// 	pb(p);
-// 	print_program(p);
-// 	pb(p);
-// 	pb(p);
-// 	print_program(p);
-// 	ft_program_close(p);
-// 	free(nums);
-// 	/* printf("{Tamaño de a: %d | Tamaño de b: %d}\n", a->size, b->size );
-// 	print_stack(a);
-// 	print_stack(b);
-// 	ft_push(a, b);
-// 	printf("----------------------------------\n");
-// 	printf("{Tamaño de a: %d | Tamaño de b: %d}\n", a->size, b->size );
-// 	print_stack(a);
-// 	print_stack(b);
-// 	ft_swap(b);
-// 	ft_swap(a);
-// 	printf("----------------------------------\n");
-// 	printf("{Tamaño de a: %d | Tamaño de b: %d}\n", a->size, b->size );
-// 	print_stack(a);
-// 	print_stack(b);
-// 	ft_rotate(a);
-// 	ft_reverse_rotate(b);
-// 	printf("----------------------------------\n");
-// 	printf("{Tamaño de a: %d | Tamaño de b: %d}\n", a->size, b->size );
-// 	print_stack(a);
-// 	print_stack(b);
-// 	ft_free_stack(a);
-// 	ft_free_stack(b); */
-// 	return (0);
-// }
-
+#include "../../libft/libft.h"
 #include "../../include/algorithms.h"
 #include "../../include/debug.h"
 #include "../../include/libraries.h"
 #include "../../include/parse.h"
 #include "../../include/stack.h"
+
+void	solve_program(t_program *p, t_parse_opts *o)
+{
+	ft_printf("Inicializando en modo: %d", o->strategy);
+	if (o->strategy == 1)
+		selection_sort(p);
+	else if (o->strategy == 2)
+		ft_printf("Algoritmo --medium-- en proceso");
+	else if (o->strategy == 3)
+		k_sort(p);
+	else if (o->strategy == 4)
+		ft_printf("Algoritmo --adaptative-- en proceso");
+	
+	if(o->bench)
+		print_benchmark(p->bench);
+}
+
 
 int	main(int argc, char **argv)
 {
-	int			*nums;
-	int			nums_size;
-	t_program	*p;
-	t_node	*min;
+	t_program		*p;
+	t_parse_data	*pd;
+	t_parse_opts	*po;
 
+	
+	pd = malloc(sizeof(t_parse_data));
+	if (!pd)
+		return (1);
+	po = malloc(sizeof(t_parse_opts));
+	if (!po)
+		return (1);
 	if (argc == 1)
 		return (0);
-	nums = NULL;
-	nums_size = 0;
-	parse_args(argc, argv, &nums, &nums_size);
-	if (!nums)
+	parse_args(argc, argv, pd, po);
+	if (!pd->array)
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
-	p = ft_program_init(nums, nums_size);
+	p = ft_program_init(pd->array, pd->size);
 	if (!p)
 		return (1);
 	print_program(p);
-	
-	
-	calculate_index(nums, p->a);
-	min = find_min(p->a);
-	print_node(min);
-	k_sort(p);
-	print_program(p);
-	print_benchmark(p->bench);
+	calculate_index(pd->array, p->a);
+	solve_program(p, po);
 	ft_program_close(p);
-	free(nums);
+	free(pd);
+	free(po);
 	return (0);
 }
