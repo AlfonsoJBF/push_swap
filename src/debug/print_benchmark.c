9c6169925c56f7ee45767e4a49d80cb02815759d
AlfonsoJBF <alfonsojbfraguio@gmail.com>
2026-07-05 00:11:28 +0200

    fix: correctly print disorder

--- src/debug/print_benchmark.c
+++ src/debug/print_benchmark.c
@@ -6,16 +6,27 @@
 /*   By: albustos <albustos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/29 17:12:49 by albustos          #+#    #+#             */
-/*   Updated: 2026/06/30 16:00:28 by albustos         ###   ########.fr       */
+/*   Updated: 2026/07/04 22:11:04 by albustos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/stack.h"
 #include "../../libft/libft.h"
 
+static void	print_disorder(int disorder)
+{
+	int percent;
+	int decimal;
+
+	percent = disorder / 100;
+	decimal =  disorder % 100;
+	ft_printf_error("[bench] disorder:  %d.%d%%\n", percent, decimal);
+	
+}
+
 void	print_benchmark(t_benchmark *bench)
 {
-	ft_printf_error("[bench] disorder:  %d%%\n",bench->disorder);
+	print_disorder(bench->disorder);
 	ft_printf_error("[bench] strategy:  %s\n",bench->strategy);
 	ft_printf_error("[bench] total_ops:  %d\n",bench->total_ops);
 
