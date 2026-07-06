a40e5b00f6f90d29fdb79038a62beb39a7a9477d
AlfonsoJBF <alfonsojbfraguio@gmail.com>
2026-07-06 15:30:08 +0200

    fix: error in printf_error, numbers were still going in stdout

--- libft/ft_printf/ft_printf_error.c
+++ libft/ft_printf/ft_printf_error.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_error.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albustos <albustos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: albustos <albustos@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 15:12:12 by mtapiado          #+#    #+#             */
-/*   Updated: 2026/06/29 18:45:55 by albustos         ###   ########.fr       */
+/*   Updated: 2026/07/06 15:25:34 by albustos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-static int	ft_putchar_printf(char c)
+static int	ft_putchar_printf_err(char c)
 {
 	ft_putchar_fd(c, 2);
 	return (1);
@@ -21,23 +21,23 @@ static int	ft_putchar_printf(char c)
 static int	ft_format(char c, va_list args)
 {
 	if (c == 'c')
-		return (ft_putchar_printf(va_arg(args, int)));
+		return (ft_putchar_printf_err(va_arg(args, int)));
 	else if (c == 's')
-		return (ft_putstring(va_arg(args, char *)));
+		return (ft_putstring_err(va_arg(args, char *)));
 	else if (c == 'd' || c == 'i')
-		return (ft_putnbr_base(va_arg(args, int), "0123456789"));
+		return (ft_putnbr_base_err(va_arg(args, int), "0123456789"));
 	else if (c == 'u')
-		return (ft_putnbr_base_un(va_arg(args, unsigned int), "0123456789"));
+		return (ft_putnbr_base_un_err(va_arg(args, unsigned int), "0123456789"));
 	else if (c == 'x')
-		return (ft_putnbr_base_un(va_arg(args, unsigned int),
+		return (ft_putnbr_base_un_err(va_arg(args, unsigned int),
 				"0123456789abcdef"));
 	else if (c == 'X')
-		return (ft_putnbr_base_un(va_arg(args, unsigned int),
+		return (ft_putnbr_base_un_err(va_arg(args, unsigned int),
 				"0123456789ABCDEF"));
 	else if (c == 'p')
-		return (ft_putpointer(va_arg(args, void *)));
+		return (ft_putpointer_err(va_arg(args, void *)));
 	else if (c == '%')
-		return (ft_putchar_printf('%'));
+		return (ft_putchar_printf_err('%'));
 	return (0);
 }
 
@@ -62,7 +62,7 @@ int	ft_printf_error(char const *format, ...)
 			count = count + ft_format(format[i], args);
 		}
 		else
-			count = count + ft_putchar_printf(format[i]);
+			count = count + ft_putchar_printf_err(format[i]);
 		i++;
 	}
 	va_end(args);
